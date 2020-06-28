01a6e208f28582870cdd837fb1b6e2fcd708b368
rikusen0335 <rikusen0335@gmail.com>
2020-06-28 11:30:35 +0900

    bump: next@9.4.4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-require('dotenv').config({ path: '.env.local'})
-//console.info(`APIKEY is ${process.env.API_KEY}`)
+console.info(`APIKEY is ${process.env.API_KEY}`)
 
 module.exports = {
-  experimental: {
-    jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
-  },
   env: {
     API_KEY: process.env.API_KEY
   }
